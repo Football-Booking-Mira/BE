8d58829f37c3e16277bdf4ca2f5d3ec7ad4ff3bc
vanh <vietcongtu66@gmail.com>
2025-10-14 23:13:59 +0700

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import routes from "./src/routes/index.js";
+import { notFoundMiddleware } from "./src/common/middlewares/notfound.middleware.js";
+import { errorMiddleware } from "./src/common/middlewares/error.middleware.js";
+import { HOST, PORT } from "./src/common/config/enviroment.js";
+import { connectDB } from "./src/common/config/database.js";
+import morgan from "morgan";
+
+connectDB();
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+
+app.use(morgan("dev"));
+
+app.use("/api", routes);
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const server = app.listen(PORT, () => {
+  console.log("API RUNNING");
+  console.log(`API: http://${HOST}:${PORT}`);
+});
+
+process.on("unhandledRejection", (error) => {
+  console.error(`Error: ${error.message}`);
+  server.close(() => process.exit(1));
+});
---
+++ src/common/config/enviroment.js
@@ -0,0 +1,5 @@
+import dotenv from "dotenv";
+
+dotenv.config({});
+
+export const { HOST, DB_URI, PORT } = process.env;
