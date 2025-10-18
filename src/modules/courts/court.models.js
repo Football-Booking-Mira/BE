fd6a1d0e0ee5359a6a3b6b6457a6fd593d4ddc16
Trinh Quoc Hung <trinhquochungwork@gmail.com>
2025-10-18 23:29:21 +0700

    add updateCourtSchema,modules amenities fix validBodyRequest,court.routes,court.controller Validate Court

--- src/modules/courts/court.models.js
+++ src/modules/courts/court.models.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { COURT_STATUS, COURT_TYPES } from '../../common/constants/enums.js';
+
 const courtSchema = new mongoose.Schema(
     {
         code: {
@@ -11,39 +12,66 @@ const courtSchema = new mongoose.Schema(
         name: {
             type: String,
             required: true,
-            unique: true,
+            trim: true,
         },
         type: {
             type: String,
             enum: Object.values(COURT_TYPES),
             default: COURT_TYPES.INDOOR,
+            required: true,
         },
         status: {
             type: String,
             enum: Object.values(COURT_STATUS),
             default: COURT_STATUS.ACTIVE,
+            required: true,
         },
         basePrice: {
             type: Number,
             required: true,
+            min: 0,
         },
         peakPrice: {
             type: Number,
             required: true,
+            min: 0,
         },
         formats: {
             type: String,
+            default: '',
         },
         description: {
             type: String,
-        },
-        deleteAt: {
-            type: Date,
-            default: null,
+            default: '',
         },
     },
-    { timestamps: true }
+    {
+        timestamps: true,
+    }
 );
-//courtSchema.index({ code: 1 }, { unique: true });
+//filter và tìm kiếm
+courtSchema.index({ code: 1 });
+courtSchema.index({ status: 1, type: 1 });
+const courtAmenitySchema = new mongoose.Schema(
+    {
+        courtId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Court',
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+//Không được trùng
+courtAmenitySchema.index({ courtId: 1, name: 1 }, { unique: true });
 const Court = mongoose.model('Court', courtSchema);
-export default Court;
+const CourtAmenity = mongoose.model('CourtAmenity', courtAmenitySchema);
+
+export { Court, CourtAmenity };
--- src/modules/courts/court.routes.js
+++ src/modules/courts/court.routes.js
@@ -8,17 +8,16 @@ import {
     updateCourt,
 } from './court.controller.js';
 import validBodyrequest from '../../common/middlewares/validBodyRequest.js';
-import courtSchema from './court.schema.js';
+import { courtSchema, updateCourtSchema } from './court.schema.js';
 
 const routesCourt = Router();
 
 routesCourt.get('/', getListCourts);
 routesCourt.get('/:id', getDetailCourt);
-
 routesCourt.delete('/:id', deleteCourt);
 routesCourt.delete('/soft-delete/:id', softDeleteCourt);
 
-routesCourt.use(validBodyrequest(courtSchema));
-routesCourt.post('/', createCourt);
-routesCourt.patch('/:id', updateCourt);
+routesCourt.post('/', validBodyrequest(courtSchema), createCourt);
+routesCourt.patch('/:id', validBodyrequest(updateCourtSchema), updateCourt);
+
 export default routesCourt;
