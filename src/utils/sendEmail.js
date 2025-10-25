01c2836e50acbbac96355be2d2ebd3fe601b87b2
vietanh <vietcongtu66@gmail.com>
2025-10-25 07:46:26 +0700

    thêm xác thực email

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -22,7 +22,6 @@ const sendMail = async ({ to, subject, html }) => {
     };
 
     const info = await transporter.sendMail(mailOptions);
-    console.log("Email sent:", info.messageId);
     return info;
   } catch (error) {
     console.error("Error sending email:", error);
