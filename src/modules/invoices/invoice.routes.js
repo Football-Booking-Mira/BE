d5fe752ffd5b5af90dc1379c48e229acdba40697
Trinh Quoc Hung <trinhquochungwork@gmail.com>
2025-11-24 22:21:52 +0700

    fixx lại 1 số lỗi  ở hóa đơn và thêm api lấy hóa đơn theo booking

--- src/modules/invoices/invoice.routes.js
+++ src/modules/invoices/invoice.routes.js
@@ -1,11 +1,18 @@
 import { Router } from 'express';
-import { createInvoice, getInvoiceById, getInvoices, updateInvoiceStatus } from './invoice.controller.js';
+import {
+    createInvoice,
+    getInvoiceByBooking,
+    getInvoiceById,
+    getInvoices,
+    updateInvoiceStatus,
+} from './invoice.controller.js';
 
 const routesInvoices = Router();
 
-routesInvoices.post("/", createInvoice);
-routesInvoices.get("/", getInvoices);
-routesInvoices.get("/:id", getInvoiceById);
-routesInvoices.patch("/:id", updateInvoiceStatus);
+routesInvoices.post('/', createInvoice);
+routesInvoices.get('/', getInvoices);
+routesInvoices.get('/by-booking/:bookingId', getInvoiceByBooking);
+routesInvoices.get('/:id', getInvoiceById);
+routesInvoices.patch('/:id', updateInvoiceStatus);
 
 export default routesInvoices;
