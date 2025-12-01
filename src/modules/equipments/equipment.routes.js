3327c75b1de5364855882e52c2401ef0ac83f534
Trinh Quoc Hung <trinhquochungwork@gmail.com>
2025-12-01 19:54:18 +0700

    fix phải là role='admin' thì mới xuống các đường dẫn crud thiết bị

--- src/modules/equipments/equipment.routes.js
+++ src/modules/equipments/equipment.routes.js
@@ -1,51 +1,45 @@
-import { Router } from "express";
+import { Router } from 'express';
+import { authenticate, authorize } from '../../common/middlewares/auth.middleware.js';
+import validate from '../../common/middlewares/validBodyRequest.js';
 import {
-  authenticate,
-  authorize,
-} from "../../common/middlewares/auth.middleware.js";
-import validate from "../../common/middlewares/validBodyRequest.js";
-import {
-  createEquipment,
-  getEquipments,
-  getEquipmentDetail,
-  updateEquipment,
-  deleteEquipment,
-} from "./equipment.controller.js";
-import {
-  createEquipmentSchema,
-  updateEquipmentSchema,
-} from "./equipment.schema.js";
+    createEquipment,
+    getEquipments,
+    getEquipmentDetail,
+    updateEquipment,
+    deleteEquipment,
+} from './equipment.controller.js';
+import { createEquipmentSchema, updateEquipmentSchema } from './equipment.schema.js';
 
 const routesEquipment = Router();
 
-// routesEquipment.use(authenticate, authorize("admin"));
-// routesEquipment.get("/", getEquipments);
-// routesEquipment.post("/", validate(createEquipmentSchema), createEquipment);
-// routesEquipment.get("/:id", getEquipmentDetail);
-// routesEquipment.patch("/:id", validate(updateEquipmentSchema), updateEquipment);
-// routesEquipment.delete("/:id", deleteEquipment);
+routesEquipment.use(authenticate, authorize('admin'));
+routesEquipment.get('/', getEquipments); // hiển thị tất cả thiết bị
+routesEquipment.post('/', validate(createEquipmentSchema), createEquipment);
+routesEquipment.get('/:id', getEquipmentDetail); // xem chi tiết thiết bị
+routesEquipment.patch('/:id', validate(updateEquipmentSchema), updateEquipment);
+routesEquipment.delete('/:id', deleteEquipment);
 
-routesEquipment.get("/", getEquipments); // hiển thị tất cả thiết bị
-routesEquipment.get("/:id", getEquipmentDetail); // xem chi tiết thiết bị
+// routesEquipment.get("/", getEquipments); // hiển thị tất cả thiết bị
+// routesEquipment.get("/:id", getEquipmentDetail); // xem chi tiết thiết bị
 
-routesEquipment.post(
-  "/",
-  authenticate,
-  authorize("admin"),
-  validate(createEquipmentSchema),
-  createEquipment
-);
-routesEquipment.patch(
-  "/:id",
-  authenticate,
-  authorize("admin"),
-  validate(updateEquipmentSchema),
-  updateEquipment
-);
-routesEquipment.delete(
-  "/:id",
-  authenticate,
-  authorize("admin"),
-  deleteEquipment
-);
+// routesEquipment.post(
+//   "/",
+//   authenticate,
+//   authorize("admin"),
+//   validate(createEquipmentSchema),
+//   createEquipment
+// );
+// routesEquipment.patch(
+//   "/:id",
+//   authenticate,
+//   authorize("admin"),
+//   validate(updateEquipmentSchema),
+//   updateEquipment
+// );
+// routesEquipment.delete(
+//   "/:id",
+//   authenticate,
+//   authorize("admin"),
+//   deleteEquipment
+// );
 export default routesEquipment;
