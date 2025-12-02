5f06731ebfe30b502d5b873182b1ea1530f7d947
ThanhDat-04 <thanhdajt2410@gmail.com>
2025-12-03 00:12:07 +0700

    fix hiển thị thiết bị user

--- src/modules/equipments/equipment.routes.js
+++ src/modules/equipments/equipment.routes.js
@@ -1,45 +1,29 @@
-import { Router } from 'express';
-import { authenticate, authorize } from '../../common/middlewares/auth.middleware.js';
-import validate from '../../common/middlewares/validBodyRequest.js';
+import { Router } from "express";
 import {
-    createEquipment,
-    getEquipments,
-    getEquipmentDetail,
-    updateEquipment,
-    deleteEquipment,
-} from './equipment.controller.js';
-import { createEquipmentSchema, updateEquipmentSchema } from './equipment.schema.js';
+  authenticate,
+  authorize,
+} from "../../common/middlewares/auth.middleware.js";
+import validate from "../../common/middlewares/validBodyRequest.js";
+import {
+  createEquipment,
+  getEquipments,
+  getEquipmentDetail,
+  updateEquipment,
+  deleteEquipment,
+} from "./equipment.controller.js";
+import {
+  createEquipmentSchema,
+  updateEquipmentSchema,
+} from "./equipment.schema.js";
 
 const routesEquipment = Router();
+routesEquipment.get("/public", getEquipments); // user không cần auth
 
-routesEquipment.use(authenticate, authorize('admin'));
-routesEquipment.get('/', getEquipments); // hiển thị tất cả thiết bị
-routesEquipment.post('/', validate(createEquipmentSchema), createEquipment);
-routesEquipment.get('/:id', getEquipmentDetail); // xem chi tiết thiết bị
-routesEquipment.patch('/:id', validate(updateEquipmentSchema), updateEquipment);
-routesEquipment.delete('/:id', deleteEquipment);
+routesEquipment.use(authenticate, authorize("admin"));
+routesEquipment.get("/", getEquipments); // hiển thị tất cả thiết bị
+routesEquipment.post("/", validate(createEquipmentSchema), createEquipment);
+routesEquipment.get("/:id", getEquipmentDetail); // xem chi tiết thiết bị
+routesEquipment.patch("/:id", validate(updateEquipmentSchema), updateEquipment);
+routesEquipment.delete("/:id", deleteEquipment);
 
-// routesEquipment.get("/", getEquipments); // hiển thị tất cả thiết bị
-// routesEquipment.get("/:id", getEquipmentDetail); // xem chi tiết thiết bị
-
-// routesEquipment.post(
-//   "/",
-//   authenticate,
-//   authorize("admin"),
-//   validate(createEquipmentSchema),
-//   createEquipment
-// );
-// routesEquipment.patch(
-//   "/:id",
-//   authenticate,
-//   authorize("admin"),
-//   validate(updateEquipmentSchema),
-//   updateEquipment
-// );
-// routesEquipment.delete(
-//   "/:id",
-//   authenticate,
-//   authorize("admin"),
-//   deleteEquipment
-// );
 export default routesEquipment;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,25 +1,27 @@
-import { Router } from 'express';
-import routesCourt from '../modules/courts/court.routes.js';
-import routesBookings from '../modules/bookings/booking.routes.js';
-import authRouter from '../modules/auth/auth.route.js';
-import routesInvoices from '../modules/invoices/invoice.routes.js';
-import routerPayment from '../modules/payment/payment.routes.js';
-import routesEquipment from '../modules/equipments/equipment.routes.js';
-import routesBookingItem from '../modules/bookingItems/bookingItem.routes.js';
-import userRouter from '../modules/users/user.routes.js';
-import uploadRoutes from '../common/routes/upload.routes.js';
+import { Router } from "express";
+import routesCourt from "../modules/courts/court.routes.js";
+import routesBookings from "../modules/bookings/booking.routes.js";
+import authRouter from "../modules/auth/auth.route.js";
+import routesInvoices from "../modules/invoices/invoice.routes.js";
+import routerPayment from "../modules/payment/payment.routes.js";
+import routesEquipment from "../modules/equipments/equipment.routes.js";
+import routesBookingItem from "../modules/bookingItems/bookingItem.routes.js";
+import userRouter from "../modules/users/user.routes.js";
+import uploadRoutes from "../common/routes/upload.routes.js";
+import emailRoutes from "../modules/email/email.routes.js";
 
 const routes = Router();
 
-routes.use('/courts', routesCourt);
-routes.use('/bookings', routesBookings);
-routes.use('/auth', authRouter);
-routes.use('/invoices', routesInvoices);
-routes.use('/payment', routerPayment);
-routes.use('/equipments', routesEquipment);
-routes.use('/booking-items', routesBookingItem);
-routes.use('/users', userRouter);
+routes.use("/courts", routesCourt);
+routes.use("/bookings", routesBookings);
+routes.use("/auth", authRouter);
+routes.use("/invoices", routesInvoices);
+routes.use("/payment", routerPayment);
+routes.use("/equipments", routesEquipment);
+routes.use("/booking-items", routesBookingItem);
+routes.use("/users", userRouter);
 // upload ảnh đăng ký user,bill chuyển tiền
-routes.use('/upload', uploadRoutes);
+routes.use("/upload", uploadRoutes);
+routes.use("/email", emailRoutes);
 
 export default routes;
