9d33e1d7867ed3c7e17e834381b6a92bbb76207b
Trinh Quoc Hung <trinhquochungwork@gmail.com>
2025-12-20 23:54:06 +0700

    fix routes review chuẩn hoá đường dẫn cập nhật trạng thái

--- src/modules/review/review.routes.js
+++ src/modules/review/review.routes.js
@@ -1,6 +1,5 @@
-import { authenticate, authorize } from "../../common/middlewares/auth.middleware.js";
-import { Router } 
-from 'express';
+import { authenticate, authorize } from '../../common/middlewares/auth.middleware.js';
+import { Router } from 'express';
 const routerReview = Router();
 import {
     createReview,
@@ -12,33 +11,17 @@ import {
     getReviewDetail,
     getReviewsByCourt,
     updateReviewStatus,
-} from "./review.controller.js";
-// 
-routerReview.get("/need-review/:id", 
-    authenticate, getFieldsNeedReview);
-routerReview.delete("/:id", 
-    authenticate, deleteReview);
-routerReview.put("/:id",
-     authenticate, updateReview);
-routerReview.post("/", 
-    authenticate, createReview);
-routerReview.get("/admin/list", 
-    authenticate, authorize('admin'), adminGetReviews);
-routerReview.get("/my", 
-    authenticate, getMyReviews);
-routerReview.get(
-    "/:id",
-    authenticate,
-    getReviewDetail
-);
-routerReview.get("/court-pulic/:courtId", getReviewsByCourt);
-routerReview.patch(
-    "/:id/status",
-    authenticate,
-    authorize("admin"),
-    updateReviewStatus
-);
+} from './review.controller.js';
+//
+routerReview.get('/need-review/:id', authenticate, getFieldsNeedReview);
+routerReview.delete('/:id', authenticate, deleteReview);
+routerReview.put('/:id', authenticate, updateReview);
+routerReview.post('/', authenticate, createReview);
+routerReview.get('/admin/list', authenticate, authorize('admin'), adminGetReviews);
+routerReview.get('/my', authenticate, getMyReviews);
+routerReview.get('/:id', authenticate, getReviewDetail);
+routerReview.get('/court-pulic/:courtId', getReviewsByCourt);
+routerReview.patch('/:id/status', authenticate, authorize('admin'), updateReviewStatus);
 
-// 
-export default 
-routerReview;
+//
+export default routerReview;
